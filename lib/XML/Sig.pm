perl-net-saml2/perl-Net-SAML2

commit 1d2dac1c3eaa42359899e61323a446d0d0af3e54

Support InclusiveNamespaces element on canonicalization transform.
Only supported when XML::CanonicalizeXML is used.

lib/XML/Sig.pm
@@ -202,15 +202,39 @@ sub _get_signed_xml {
 
 sub _transform {
     my $self = shift;
-    my ($xml, $transform_algorithm_context) = @_;
-    foreach my $node ($self->{parser}->find('dsig:SignedInfo/dsig:Reference/dsig:Transforms/dsig:Transform/@Algorithm', $transform_algorithm_context)->get_nodelist) {
-        my $alg = $node->getNodeValue;
-        if ($alg eq TRANSFORM_ENV_SIG) { $xml = $self->_transform_env_sig($xml); }
-        elsif ($alg eq TRANSFORM_EXC_C14N) { $xml = $self->_canonicalize_xml($xml,0); }
-        elsif ($alg eq TRANSFORM_EXC_C14N_COMMENTS) { $xml = $self->_canonicalize_xml($xml,1); }
-        else { die "Unsupported transform: $alg"; }
+    my ($xml, $context) = @_;
+
+    my $transforms = $self->{parser}->find(
+        'dsig:SignedInfo/dsig:Reference/dsig:Transforms/dsig:Transform', 
+        $context
+    );
+
+    foreach my $node ($transforms->get_nodelist) {
+        my $alg = $node->getAttribute('Algorithm');
+
+        if ($alg eq TRANSFORM_ENV_SIG) {
+            $xml = $self->_transform_env_sig($xml);
+        }
+        elsif ($alg eq TRANSFORM_EXC_C14N) {
+            my $prefixlist = $self->_find_prefixlist($node);
+            $xml = $self->_canonicalize_xml($xml, 0, $prefixlist);
+        }
+        elsif ($alg eq TRANSFORM_EXC_C14N_COMMENTS) {
+            my $prefixlist = $self->_find_prefixlist($node);
+            $xml = $self->_canonicalize_xml($xml, 1, $prefixlist);
+        }
+        else {
+            die "Unsupported transform: $alg";
+        }
     }
     return $xml;
+}
+
+sub _find_prefixlist {
+    my $self = shift;
+    my ($node) = @_;
+    my $prefixlist = $self->{parser}->findvalue('ec:InclusiveNamespaces/@PrefixList', $node);
+    return $prefixlist;
 }
 
 sub _verify_rsa {
@@ -577,18 +601,26 @@ sub _reference_xml {
 
 sub _canonicalize_xml {
     my $self = shift;
-    my ($xml,$comments) = @_;
+    my ($xml,$comments,$prefixlist) = @_;
     $comments = 0 unless $comments;
+    $prefixlist = '' unless $prefixlist;
 
     if ( $self->{canonicalizer} eq 'XML::Canonical' ) {
         require XML::Canonical;
+
+        # TODO - pass prefixlist in here if X::C supports it
+
         my $xmlcanon = XML::Canonical->new( comments => $comments );
         return $xmlcanon->canonicalize_string( $xml );
     }
     elsif ( $self->{ canonicalizer } eq 'XML::CanonicalizeXML' ) {
         require XML::CanonicalizeXML;
         my $xpath = '<XPath>(//. | //@* | //namespace::*)</XPath>';
-        return XML::CanonicalizeXML::canonicalize( $xml, $xpath, [], 1, $comments );
+
+        # adjust prefixlist from attribute for XML::CanonicalizeXML's format
+        $prefixlist =~ s/ /,/g;
+
+        return XML::CanonicalizeXML::canonicalize( $xml, $xpath, $prefixlist, 1, $comments );
     }
     else {
         confess "Unknown XML canonicalizer module.";

perl-net-saml2/perl-XML-Sig

commit 15939aff3d14e718ecc90d5d54a100e270eb2b0d

InclusiveNamespaces support from 1d2dac1c3eaa42359899e61323a446d0d0af3e54 accidentally removed

lib/XML/Sig.pm
@@ -628,16 +628,45 @@ sub _transform {
             $xml = $xml->toStringC14N(1);
         }
         elsif ($alg eq TRANSFORM_EXC_C14N) {
-            $xml = $xml->toStringEC14N();
+            my @prefixlist = $self->_find_prefixlist($node);
+            print "    Prefixlist: @prefixlist\n" if $DEBUG;
+            $xml = $xml->toStringEC14N(0, '', \@prefixlist);
         }
         elsif ($alg eq TRANSFORM_EXC_C14N_COMMENTS) {
-            $xml = $xml->toStringEC14N(1);
+            my @prefixlist = $self->_find_prefixlist($node);
+            print "    Prefixlist: @prefixlist\n" if $DEBUG;
+            $xml = $xml->toStringEC14N(1, '', \@prefixlist);
         }
         else {
             die "Unsupported transform: $alg";
         }
     }
     return $xml;
+}
+
+##
+## _find_prefixlist($node)
+##
+## Arguments:
+##    $node:    string XML NodeSet
+##
+## Returns: ARRAY of prefix lists
+##
+## Generate an array of prefix lists defined in InclusiveNamespaces
+##
+sub _find_prefixlist {
+    my $self = shift;
+    my ($node) = @_;
+    my @children = $node->getChildrenByTagName('InclusiveNamespaces');
+
+    my $prefixlist = '';
+    foreach my $child (@children) {
+        if ($child) {
+            $prefixlist .= $child->getAttribute('PrefixList');
+        }
+        $prefixlist .= ' ';
+    }
+    return split / /, $prefixlist;
 }
 
 ##