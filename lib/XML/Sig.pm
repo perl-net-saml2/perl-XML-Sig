8f0738f2eec638760105d039d7cfc88022b0638b
Timothy Legge <timlegge@gmail.com>
2021-01-12 23:03:30 +0000

    Fixes #20 Remove embedded newlines from reference digest

--- lib/XML/Sig.pm
+++ lib/XML/Sig.pm
@@ -389,8 +389,10 @@ sub verify {
         print ("   Digest Method: $digest_method\n") if $DEBUG;
 
         # Get the DigestValue used to verify Canonical XML
-        my $refdigest = _trim($self->{ parser }->findvalue(
-                'dsig:SignedInfo/dsig:Reference/dsig:DigestValue', $signature_node));
+        # Note that the digest may have embedded newlines in the XML
+        # Decode the base64 and encode it with no newlines
+        my $refdigest = encode_base64(decode_base64(_trim($self->{ parser }->findvalue(
+                'dsig:SignedInfo/dsig:Reference/dsig:DigestValue', $signature_node))), "");
         print ("   Digest Value: $refdigest\n") if $DEBUG;
 
         # Get the SignatureValue used to verify the SignedInfo
@@ -483,7 +485,7 @@ sub verify {
         # Obtain the DigestValue of the Canonical XML
         my $digest = $self->{digest_method}->($canonical);
 
-        print ( "    Reference Digest " . _trim($refdigest) ."\n") if $DEBUG;
+        print ( "    Reference Digest:  " . _trim($refdigest) ."\n") if $DEBUG;
 
         print ( "    Calculated Digest: ". _trim(encode_base64($digest, '')) ."\n") if $DEBUG;
 
