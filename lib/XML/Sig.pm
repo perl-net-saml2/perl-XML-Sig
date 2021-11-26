da1e45bd7058461bf1426730f4a996d823e230c5
Timothy Legge <timlegge@gmail.com>
2021-11-26 12:32:06 -0400

    Fixed #38 - Issue verifying XML that includes wide characters

--- lib/XML/Sig.pm
+++ lib/XML/Sig.pm
@@ -3,6 +3,7 @@ package XML::Sig;
 use strict;
 use warnings;
 
+use Encode;
 # ABSTRACT: XML::Sig - A toolkit to help sign and verify XML Digital Signatures
 =head1 NAME
 
@@ -351,7 +352,7 @@ sub sign {
         }
 
         # Calculate the digest of the XML being signed
-        my $bin_digest    = $self->{digest_method}->( $xml_canon );
+        my $bin_digest    = $self->{digest_method}->( Encode::encode_utf8( $xml_canon ));
         my $digest        = encode_base64( $bin_digest, '' );
         print ("   Digest: $digest\n") if $DEBUG;
 
@@ -599,8 +600,10 @@ sub verify {
         # signatures to be validated if they exist
         $signed_xml->addChild( $signature_node );
 
+        print ( "    Canonical XML:  " . $canonical ."\n") if $DEBUG;
+
         # Obtain the DigestValue of the Canonical XML
-        my $digest = $self->{digest_method}->($canonical);
+        my $digest = $self->{digest_method}->(Encode::encode_utf8($canonical));
 
         print ( "    Reference Digest:  " . _trim($refdigest) ."\n") if $DEBUG;
 
