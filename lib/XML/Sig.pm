b3efb7342c02c81d2aa4448acded48a2939d4652
Wesley Schwengle <waterkip@cpan.org>
2023-06-18 11:45:48 -0400

    Allow verification on one node:
    The following changeset implements the following equivalent of xmlsec1:
    
        /usr/bin/xmlsec1 --verify --id-attr:ID \
            urn:oasis:names:tc:SAML:2.0:protocol:ArtifactResponse
    
    Signed-off-by: Wesley Schwengle <waterkip@cpan.org>

--- lib/XML/Sig.pm
+++ lib/XML/Sig.pm
@@ -464,7 +464,7 @@ Returns: string  Signed XML
 sub verify {
     my $self = shift;
     delete $self->{signer_cert};
-    my ($xml) = @_;
+    my $xml = shift;
 
     my $dom = XML::LibXML->load_xml( string => $xml );
 
@@ -475,6 +475,16 @@ sub verify {
     $self->{ parser }->registerNs('ecdsa', 'http://www.w3.org/2001/04/xmldsig-more#');
 
     my $signature_nodeset = $self->{ parser }->findnodes('//dsig:Signature');
+
+    my $key_to_verify;
+    if ($self->{id_attr}) {
+        if ($self->{ns}) {
+            foreach (keys %{$self->{ns}}) {
+                $self->{ parser }->registerNs($_, $self->{ns}{$_});
+            }
+        }
+        $key_to_verify = $self->_get_ids_to_sign();
+    }
 
     my $numsigs = $signature_nodeset->size();
     print ("NodeSet Size: $numsigs\n") if $DEBUG;
@@ -490,7 +500,12 @@ sub verify {
             'dsig:SignedInfo/dsig:Reference/@URI', $signature_node);
         $reference =~ s/#//g;
 
-        print ("   Reference URI: $reference\n") if $DEBUG;
+        print("   Reference URI: $reference\n") if $DEBUG;
+
+        if ($key_to_verify && $key_to_verify ne $reference) {
+            print ("Skipping reference URI: $reference, does not match required option\n") if $DEBUG;
+            next;
+        }
 
         # The reference ID must point to something in the document
         # if not disregard it and look for another signature
